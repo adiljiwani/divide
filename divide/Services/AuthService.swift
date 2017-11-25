6400c98970d1108430f8fe3247069a702e5d7171
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-25 16:21:37 -0500

    inset text field and border for buttons

---
+++ divide/Services/AuthService.swift
@@ -0,0 +1,14 @@
+//
+//  AuthService.swift
+//  divide
+//
+//  Created by Adil Jiwani on 2017-11-25.
+//  Copyright © 2017 Adil Jiwani. All rights reserved.
+//
+
+import Foundation
+
+class AuthService {
+    static let instance = AuthService()
+    
+}
