0bb5bdaa4a6b742bdb05d22e0bce8dba67a554ed
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-26 03:02:05 -0500

    tab bar

--- divide/Controller/AuthVC.swift
+++ divide/Controller/AuthVC.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class AuthVC: UIViewController {
 
+    @IBOutlet weak var passTxtField: InsetTextField!
+    @IBOutlet weak var emailTxtField: InsetTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        emailTxtField.delegate = self
+        passTxtField.delegate = self
     }
 
+    @IBAction func loginPressed(_ sender: Any) {
+        if emailTxtField.text != nil && passTxtField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailTxtField.text!, andPassword: passTxtField.text!, loginComplete: { (success, loginError) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                }
+                
+                AuthService.instance.registerUser(withEmail: self.emailTxtField.text!, andPassword: self.passTxtField.text!, userCreationComplete: { (success, registrationError) in
+                    if success {
+                        AuthService.instance.loginUser(withEmail: self.emailTxtField.text!, andPassword: self.passTxtField.text!, loginComplete: { (success, nil) in
+                            self.dismiss(animated: true, completion: nil)
+                            print("Successfully registered user.")
+                        })
+                    } else {
+                        print(String(describing: registrationError?.localizedDescription))
+                    }
+                })
+            })
+        }
+    }
+    
+}
+
+extension AuthVC: UITextFieldDelegate {
+    
 }
