ea5c006d24f57f41a3f05a5371a513bb1382d651
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-12-06 14:05:57 -0500

    take photo or upload image and read text

--- divide/Controller/CameraVC.swift
+++ divide/Controller/CameraVC.swift
@@ -8,77 +8,108 @@
 
 import UIKit
 import AVFoundation
+import TesseractOCR
 
 class CameraVC: UIViewController {
-
-    var captureSession: AVCaptureSession!
-    var cameraOutput: AVCapturePhotoOutput!
-    var previewLayer: AVCaptureVideoPreviewLayer!
-
-    var photoData: Data?
     
-        @IBOutlet weak var captureImageView: UIImageView!
-    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var takePhotoBtn: RoundedButton!
+    
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.isHidden = true
+        takePhotoBtn.setTitle("Take Photo / Upload Image", for: .normal)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        previewLayer.frame = cameraView.bounds
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        captureSession = AVCaptureSession()
-        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
-        
-        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: backCamera!)
-            if captureSession.canAddInput(input) {
-                captureSession.addInput(input)
-            }
-            
-            cameraOutput = AVCapturePhotoOutput()
-            if captureSession.canAddOutput(cameraOutput){
-                captureSession.addOutput(cameraOutput!)
-                
-                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
-                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-                
-                cameraView.layer.addSublayer(previewLayer!)
-                captureSession.startRunning()
-            }
-        } catch {
-            debugPrint(error)
+    func performImageRecognition(_ image: UIImage) {
+        if let tesseract = G8Tesseract(language: "eng+fra") {
+            // 2
+            tesseract.engineMode = .tesseractCubeCombined
+            // 3
+            tesseract.pageSegmentationMode = .auto
+            // 4
+            tesseract.image = image.g8_blackAndWhite()
+            // 5
+            tesseract.recognize()
+            // 6
+            textView.text = tesseract.recognizedText
         }
+        // 7
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
     }
     
-    @IBAction func capturePressed(_ sender: Any) {
-        let settings = AVCapturePhotoSettings()
-        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
-        
-        cameraOutput.capturePhoto(with: settings, delegate: self)
+    @IBAction func takePhoto(_ sender: Any) {
+        presentImagePicker()
     }
-    
-    @IBAction func backPressed(_ sender: Any) {
-        
-    }
-    
 }
 
-extension CameraVC: AVCapturePhotoCaptureDelegate {
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        if let error = error {
-            debugPrint(error)
-        } else {
-            photoData = photo.fileDataRepresentation()
-            
-            let image = UIImage(data: photoData!)
-            self.captureImageView.image = image
+extension CameraVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func presentImagePicker() {
+        let imagePickerActionSheet = UIAlertController(title: "Take Photo/Upload Image",
+                                                       message: nil, preferredStyle: .actionSheet)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraButton = UIAlertAction(title: "Take Photo",
+                                             style: .default) { (alert) -> Void in
+                                                let imagePicker = UIImagePickerController()
+                                                imagePicker.delegate = self
+                                                imagePicker.sourceType = .camera
+                                                self.present(imagePicker, animated: true)
+            }
+            imagePickerActionSheet.addAction(cameraButton)
+        }
+        // 1
+        let libraryButton = UIAlertAction(title: "Choose from Camera Roll",
+                                          style: .default) { (alert) -> Void in
+                                            let imagePicker = UIImagePickerController()
+                                            imagePicker.delegate = self
+                                            imagePicker.sourceType = .photoLibrary
+                                            self.present(imagePicker, animated: true)
+        }
+        imagePickerActionSheet.addAction(libraryButton)
+        // 2
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        imagePickerActionSheet.addAction(cancelButton)
+        // 3
+        present(imagePickerActionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // 2
+        if let selectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage,
+            let scaledImage = selectedPhoto.scaleImage(640) {
+            // 3
+            activityIndicator.startAnimating()
+            activityIndicator.isHidden = false
+            takePhotoBtn.setTitle("Take Photo / Upload Image", for: .normal)
+            // 4
+            dismiss(animated: true, completion: {
+                self.performImageRecognition(scaledImage)
+            })
         }
     }
 }
+
+extension UIImage {
+    func scaleImage(_ maxDimension: CGFloat) -> UIImage? {
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        
+        if size.width > size.height {
+            let scaleFactor = size.height / size.width
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            let scaleFactor = size.width / size.height
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        draw(in: CGRect(origin: .zero, size: scaledSize))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+}
+
