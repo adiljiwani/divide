131d935ac9e9a9fcf03c77612d14be4b06c7e2ab
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-12-12 23:07:37 -0500

    group details vc initialized

---
+++ divide/Controller/GroupDetailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  GroupDetailsVC.swift
+//  divide
+//
+//  Created by Adil Jiwani on 2017-12-12.
+//  Copyright © 2017 Adil Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class GroupDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
