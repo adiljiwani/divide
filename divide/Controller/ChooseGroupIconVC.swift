cd1e22f0a2532cb22287884b1090b9b261e1d52e
Adil Jiwani <a8jiwani@uwaterloo.ca>
2018-01-24 19:49:12 -0500

    choose group icon vc created and ui done

---
+++ divide/Controller/ChooseGroupIconVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseGroupIconVC.swift
+//  divide
+//
+//  Created by Adil Jiwani on 2018-01-24.
+//  Copyright © 2018 Adil Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class ChooseGroupIconVC: UIViewController {
+    fileprivate let sectionInsets = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
