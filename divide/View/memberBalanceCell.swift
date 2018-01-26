85917bf0445d5aa85715aab962d29d130376d7eb
Adil Jiwani <a8jiwani@uwaterloo.ca>
2018-01-26 18:48:12 -0500

    initialized member balance cell

---
+++ divide/View/memberBalanceCell.swift
@@ -0,0 +1,13 @@
+//
+//  memberBalanceCell.swift
+//  divide
+//
+//  Created by Adil Jiwani on 2018-01-26.
+//  Copyright © 2018 Adil Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class memberBalanceCell: UICollectionViewCell {
+    
+}
